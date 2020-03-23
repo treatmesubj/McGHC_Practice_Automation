d68c10ddb29aad3c8ddfd23e97fc6ae033d9eb81
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-03-23 15:55:59 -0500

    Add files via upload

---
+++ Connect_Correct.js
@@ -0,0 +1,25 @@
+var questions_left;
+questions_left = 2;
+
+function auto_correct() {
+	var iframe = document.querySelector('iframe');
+	var nested_iframe = iframe.contentDocument.body.querySelector("div[style*='position'][style*='display: block'] iframe");
+	var magic_correct = nested_iframe.contentDocument.body.querySelector("[aria-label='Magic Correct']");
+	var questions_left_elem = nested_iframe.contentDocument.body.querySelector("[aria-label*='items left']");
+	questions_left = parseInt(questions_left_elem.getAttribute('aria-label').split(" ")[2]);
+	console.log(questions_left)
+	magic_correct.click()
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function loop() {
+	while  (questions_left > 1) {
+		auto_correct()
+		await sleep(((Math.random() + 1)*1000)
+	}
+}
+
+loop()
