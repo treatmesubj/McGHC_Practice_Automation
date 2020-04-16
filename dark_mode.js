c24ea9a533a5f49f46d25dbeed6f03e1c8284e85
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-04-16 10:28:16 -0500

    dang nested iframes

--- dark_mode.js
+++ dark_mode.js
@@ -1,4 +1,5 @@
-var iframe = document.querySelector("div[style*='block'] iframe");
-iframe.contentDocument.body.querySelector("div[role='banner']").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
-iframe.contentDocument.body.querySelector("div[role='banner'] + div").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
-iframe.contentDocument.body.querySelector("div[role='complementary']").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
+var iframe = document.querySelector('iframe');
+var nested_iframe = iframe.contentDocument.body.querySelector("div[style*='position'][style*='display: block'] iframe");
+nested_iframe.contentDocument.body.querySelector("div[role='banner']").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
+nested_iframe.contentDocument.body.querySelector("div[role='banner'] + div").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
+nested_iframe.contentDocument.body.querySelector("div[role='complementary']").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
