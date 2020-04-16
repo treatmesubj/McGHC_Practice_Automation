9843c78f5d2d296e1ab215294478d6455a39702c
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-04-16 10:17:37 -0500

    refactor

--- Connect_Correct.js
+++ Connect_Correct.js
@@ -3,7 +3,8 @@ questions_left = 2;
 
 function auto_correct() {
   var iframe = document.querySelector('iframe');
-  if (iframe.contentWindow.document.body.querySelector('[aria-label="Magic Correct"]') === null) {
+  var magic_correct = iframe.contentWindow.document.body.querySelector('[aria-label="Magic Correct"]')
+  if (magic_correct === null) {
     var nested_iframe = iframe.contentDocument.body.querySelector("div[style*='position'][style*='display: block'] iframe");
     var magic_correct = nested_iframe.contentDocument.body.querySelector("[aria-label='Magic Correct']");
     var questions_left_elem = nested_iframe.contentDocument.body.querySelector("[aria-label*='items left']");
@@ -11,7 +12,6 @@ function auto_correct() {
     console.log(questions_left)
     magic_correct.click()
   } else {
-    var magic_correct = iframe.contentDocument.body.querySelector("[aria-label='Magic Correct']");
     var questions_left_elem = iframe.contentDocument.body.querySelector("[aria-label*='items left']");
     questions_left = parseInt(questions_left_elem.getAttribute('aria-label').split(" ")[2]);
     console.log(questions_left)
